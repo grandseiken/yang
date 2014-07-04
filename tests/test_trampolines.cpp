bc78f1ed531645d8f113c85a5d0ae8719e106f02
Stuart Taylor <github@seiken.co.uk>
2014-07-04 17:37:34 +0100

    Split out the tests in test_trampolines.cpp.

--- tests/test_trampolines.cpp
+++ tests/test_trampolines.cpp
@@ -8,32 +8,87 @@ namespace yang {
 
 struct TrampolinesTest : YangTest {};
 
-const std::string TestTrampolinesStr = R"(
-export test_int = int(int x)
+TEST_F(TrampolinesTest, ForwardInt)
+{
+  auto inst = instance(R"(
+export f = int(int x)
 {
   return x * 2;
 }
-export test_float = float(float x)
+)");
+  EXPECT_EQ(10, inst.call<int_t>("f", 5));
+}
+
+TEST_F(TrampolinesTest, ForwardFloat)
+{
+  auto inst = instance(R"(
+export f = float(float x)
 {
   return x * 2.;
 }
-export test_int4 = int4(int4 x)
+)");
+  EXPECT_EQ(1. / 4, inst.call<float_t>("f", 1. / 8));
+}
+
+TEST_F(TrampolinesTest, ForwardIvec)
+{
+  auto inst = instance(R"(
+export f = int4(int4 x)
 {
   return x * 2;
 }
-export test_float4 = float4(float4 x)
+)");
+  ivec_t<4> in{1, 2, 3, 4};
+  ivec_t<4> out{2, 4, 6, 8};
+  EXPECT_EQ(out, inst.call<ivec_t<4>>("f", in));
+}
+
+TEST_F(TrampolinesTest, ForwardFvec)
+{
+  auto inst = instance(R"(
+export f = float4(float4 x)
 {
   return x * 2.;
 }
-export test_function = int(int)(int(int) x)
+)");
+  fvec_t<4> in{1. / 8, 1. / 4, 1. / 2, 1.};
+  fvec_t<4> out{1. / 4, 1. / 2, 1., 2.};
+  EXPECT_EQ(out, inst.call<fvec_t<4>>("f", in));
+}
+
+TEST_F(TrampolinesTest, ForwardFunction)
+{
+  typedef Function<int_t(int_t)> intf_t;
+  auto inst = instance(R"(
+export f = int(int)(int(int) x)
 {
   return x;
 }
-export test_user_type = UserType(UserType x)
+export g = int(int x)
+{
+  return x * 2;
+}
+)");
+  auto g = inst.get_function<intf_t>("g");
+  EXPECT_EQ(g(1), inst.call<intf_t>("f", g)(1));
+}
+
+TEST_F(TrampolinesTest, ForwardUserType)
+{
+  auto inst = instance(R"(
+export f = UserType(UserType x)
 {
   return x;
 }
-export test_muser_type = MuserType(MuserType x)
+)");
+  user_type u;
+  EXPECT_EQ(&u, inst.call<user_type*>("f", &u));
+}
+
+TEST_F(TrampolinesTest, ForwardManagedUserType)
+{
+  auto inst = instance(R"(
+export f = MuserType(MuserType x)
 {
   return x;
 }
@@ -41,142 +96,155 @@ export get_muser_type = MuserType()
 {
   return MuserType();
 }
-)";
-
-TEST_F(TrampolinesTest, Forward)
-{
-  auto inst = instance(TestTrampolinesStr);
-  EXPECT_EQ(10, inst.call<int_t>("test_int", 5));
-  EXPECT_EQ(1.0 / 4, inst.call<float_t>("test_float", 1.0 / 8));
-
-  ivec_t<4> ivec{1, 2, 3, 4};
-  EXPECT_EQ(2 * ivec, inst.call<ivec_t<4>>("test_int4", ivec));
-
-  fvec_t<4> fvec{1.0 / 8, 1.0 / 4, 1.0 / 2, 1.0};
-  EXPECT_EQ(2. * fvec, inst.call<fvec_t<4>>("test_float4", fvec));
-
-  typedef Function<int_t(int_t)> intf_t;
-  auto test_int = inst.get_function<intf_t>("test_int");
-  auto r = inst.call<intf_t>("test_function", test_int);
-  EXPECT_EQ(r(1), test_int(1));
-
-  user_type u;
-  EXPECT_EQ(&u, inst.call<user_type*>("test_user_type", &u));
-
-  auto m = inst.call<Ref<muser_type>>("get_muser_type");
-  EXPECT_EQ(m->id, inst.call<Ref<muser_type>>("test_muser_type", m)->id);
+)");
+  auto u = inst.call<Ref<muser_type>>("get_muser_type");
+  EXPECT_EQ(u->id, inst.call<Ref<muser_type>>("f", u)->id);
 }
 
-const std::string TestReverseTrampolinesStr = R"(
-export test_int = int()
+TEST_F(TrampolinesTest, ReverseInt)
 {
-  return context_int(5);
-}
-export test_float = float()
-{
-  return context_float(5.);
-}
-export test_int4 = int4()
-{
-  return context_int4((0, -1, -2, -3));
-}
-export test_float4 = float4()
-{
-  return context_float4((-.5, -1., -1.5, -2.));
-}
-export test_function = int(int)()
-{
-  return context_function(int(int x) {return x * 2;});
-}
-export test_user_type = UserType()
-{
-  return context_user_type(get_user_type());
-}
-export test_muser_type = MuserType()
-{
-  return context_muser_type(MuserType());
-}
-)";
-
-TEST_F(TrampolinesTest, Reverse)
-{
-  typedef Function<int_t(int_t)> intf_t;
-  auto context_int = [](int_t x)
-  {
-    return x;
-  };
-  auto context_float = [](float_t x)
-  {
-    return x;
-  };
-  auto context_int4 = [](ivec_t<4> x)
-  {
-    return x;
-  };
-  auto context_float4 = [](fvec_t<4> x)
-  {
-    return x;
-  };
-  auto context_function = [](intf_t x)
-  {
-    return x;
-  };
-  auto context_user_type = [](user_type* x)
-  {
-    return x;
-  };
-  auto context_muser_type = [](Ref<muser_type> x)
-  {
-    return x;
-  };
-
   auto ctxt = context();
-  ctxt.register_function("context_int", make_fn(context_int));
-  ctxt.register_function("context_float", make_fn(context_float));
-  ctxt.register_function("context_int4", make_fn(context_int4));
-  ctxt.register_function("context_float4", make_fn(context_float4));
-  ctxt.register_function("context_function", make_fn(context_function));
-  ctxt.register_function("context_user_type", make_fn(context_user_type));
-  ctxt.register_function("context_muser_type", make_fn(context_muser_type));
+  ctxt.register_function("context", make_fn([](int_t x)
+  {
+    return x;
+  }));
 
-  auto inst = instance(ctxt, TestReverseTrampolinesStr);
-  EXPECT_EQ(5, inst.call<int_t>("test_int"));
-  EXPECT_EQ(5., inst.call<float_t>("test_float"));
-  EXPECT_EQ(ivec_t<4>(0, -1, -2, -3), inst.call<ivec_t<4>>("test_int4"));
-  EXPECT_EQ(fvec_t<4>(-.5, -1., -1.5, -2.),
-            inst.call<fvec_t<4>>("test_float4"));
-  EXPECT_EQ(32, inst.call<intf_t>("test_function")(16));
-  EXPECT_EQ(0, inst.call<user_type*>("test_user_type")->id);
-  EXPECT_EQ(0, inst.call<Ref<muser_type>>("test_muser_type")->id);
-}
-
-const std::string TestMultiArgTrampolinesStr = R"(
-export test = int(int a, int4 b, float c, int(int) d, float2 e, UserType f)
+  auto inst = instance(ctxt, R"(
+export f = int()
 {
-  return context_test(a, b, c, d, e, f);
+  return context(5);
 }
-)";
+)");
+  EXPECT_EQ(5, inst.call<int_t>("f"));
+}
+
+TEST_F(TrampolinesTest, ReverseFloat)
+{
+  auto ctxt = context();
+  ctxt.register_function("context", make_fn([](float_t x)
+  {
+    return x;
+  }));
+
+  auto inst = instance(ctxt, R"(
+export f = float()
+{
+  return context(5.);
+}
+)");
+  EXPECT_EQ(5., inst.call<float_t>("f"));
+}
+
+TEST_F(TrampolinesTest, ReverseIvec)
+{
+  auto ctxt = context();
+  ctxt.register_function("context", make_fn([](ivec_t<4> x)
+  {
+    return x;
+  }));
+
+  auto inst = instance(ctxt, R"(
+export f = int4()
+{
+  return context((0, -1, -2, -3));
+}
+)");
+  EXPECT_EQ(ivec_t<4>(0, -1, -2, -3), inst.call<ivec_t<4>>("f"));
+}
+
+TEST_F(TrampolinesTest, ReverseFvec)
+{
+  auto ctxt = context();
+  ctxt.register_function("context", make_fn([](fvec_t<4> x)
+  {
+    return x;
+  }));
+
+  auto inst = instance(ctxt, R"(
+export f = float4()
+{
+  return context((-.5, -1., -1.5, -2.));
+}
+)");
+  EXPECT_EQ(fvec_t<4>(-.5, -1., -1.5, -2.), inst.call<fvec_t<4>>("f"));
+}
+
+TEST_F(TrampolinesTest, ReverseFunction)
+{
+  typedef Function<int_t(int_t)> intf_t;
+  auto ctxt = context();
+  ctxt.register_function("context", make_fn([](intf_t x)
+  {
+    return x;
+  }));
+
+  auto inst = instance(ctxt, R"(
+export f = int(int)()
+{
+  return context(int(int x) {return x * 2;});
+}
+)");
+  EXPECT_EQ(32, inst.call<intf_t>("f")(16));
+}
+
+TEST_F(TrampolinesTest, ReverseUserType)
+{
+  auto ctxt = context();
+  ctxt.register_function("context", make_fn([](user_type* x)
+  {
+    return x;
+  }));
+
+  auto inst = instance(ctxt, R"(
+export f = UserType()
+{
+  return context(get_user_type());
+}
+)");
+  EXPECT_EQ(0, inst.call<user_type*>("f")->id);
+}
+
+TEST_F(TrampolinesTest, ReverseManagedUserType)
+{
+  auto ctxt = context();
+  ctxt.register_function("context", make_fn([](Ref<muser_type> x)
+  {
+    return x;
+  }));
+
+  auto inst = instance(ctxt, R"(
+export f = MuserType()
+{
+  return context(MuserType());
+}
+)");
+  EXPECT_EQ(0, inst.call<Ref<muser_type>>("f")->id);
+}
 
 TEST_F(TrampolinesTest, MultiArg)
 {
-  auto ctxt = context();
   typedef Function<int_t(int_t)> intf_t;
-  auto context_test = [](
+  auto ctxt = context();
+  ctxt.register_function("context", make_fn([](
       int_t a, ivec_t<4> b, float_t c, intf_t d, fvec_t<2> e, user_type* f)
   {
     return d(a) + b[0] + b[1] + b[2] + b[3] +
         int_t(c) + int_t(e[x]) + int_t(e[y]) + int_t(f->id);
-  };
-  ctxt.register_function("context_test", make_fn(context_test));
+  }));
+  auto inst = instance(ctxt, R"(
+export f = int(int a, int4 b, float c, int(int) d, float2 e, UserType f)
+{
+  return context(a, b, c, d, e, f);
+}
+)");
 
-  auto d = [](int_t a)
+  auto d = make_fn([](int_t a)
   {
     return a * 2;
-  };
+  });
   user_type u{12};
-  int_t result = instance(ctxt, TestMultiArgTrampolinesStr).call<int_t>(
-      "test",
-      2, ivec_t<4>{1, 2, 3, 4}, 3.5, make_fn(d), fvec_t<2>{7.1, 6.9}, &u);
+  int_t result = inst.call<int_t>(
+      "f", 2, ivec_t<4>{1, 2, 3, 4}, 3.5, d, fvec_t<2>{7.1, 6.9}, &u);
   EXPECT_EQ(42, result);
 }
 
